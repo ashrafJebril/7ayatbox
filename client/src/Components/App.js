6ce1aab187c1738a981a5e27d568aff8c4fa94a2
mahmoud khudairi <mahmoudkhudairi@gmail.com>
2018-12-23 12:24:05 +0200

    (fix) react router in appjs and homejs

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import $ from "jquery";
-import Home from '../Components/Home/Home'
-import Nav from '../Components/Home/NavBar/Nav'
-import Footer from '../Components/Home/Footer/Footer'
-import Categories from '../Components/Categories/Categories'
-import SignIn from '../Components/SignIn/SignIn'
-
-
-import {BrowserRouter ,Route ,Switch} from 'react-router-dom'
+import Home from "../Components/Home/Home";
+import Nav from "../Components/Home/NavBar/Nav";
+import Footer from "../Components/Home/Footer/Footer";
+import Categories from "../Components/Categories/Categories";
+import SignIn from "../Components/SignIn/SignIn";
+import Budget from "../Components/Categories/Budget/Budget";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -20,11 +19,12 @@ class App extends Component {
       type: "POST",
       data: { email: "mahmoud@gmail.com", password: "123456", name: "mahmoud" },
       success: data => {
-        alert("Hi");
+        //alert("Hi");
+        console.log("success");
         this.setState({ text: data.express });
       },
       error: err => {
-        alert(err);
+        console.log("ERROR");
       }
     });
   }
@@ -34,16 +34,15 @@ class App extends Component {
         <Nav />
 
         <BrowserRouter>
-        <Switch>
-        <Route path="/SignIn" component={SignIn} exact/>
-        <Route path="/" component={Home} exact/>
-        <Route path='/Categories' component={Categories} exact />
-        </Switch>
-        
+          <Switch>
+            <Route exact path="/SignIn" component={SignIn} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Categories" component={Categories} />
+            <Route exact path="/budget" component={Budget} />
+          </Switch>
         </BrowserRouter>
-<Footer/>
-
-</div>
+        <Footer />
+      </div>
     );
   }
 }
--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,49 +1,48 @@
-import React, { Component } from 'react';
-import "./Home.css"
-import "./About/About"
-import About from './About/About';
-
+import React, { Component } from "react";
+import "./Home.css";
+import "./About/About";
+import About from "./About/About";
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = ""
+    this.state = "";
   }
 
   render() {
-    return(
-    <div>
-    <header className="container-fluid index-header">
-		<div className="row title-row">
-    <div className="col-12">
-    <div className="backgroundImg"></div>
-  </div>
-  
-</div>
+    return (
+      <div>
+        <header className="container-fluid index-header">
+          <div className="row title-row">
+            <div className="col-12">
+              <div className="backgroundImg" />
+            </div>
+          </div>
 
-<div className="row header-screen-row">
-  <div className="screen-header-controllers">
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-  
-  
-  
-  <div className="col-12 title-row">
-    <h1><mark>Create Your Beautiful Wedding </mark></h1>
-    <h4><mark>Be Your Wedding Planner</mark></h4>
+          <div className="row header-screen-row">
+            <div className="screen-header-controllers">
+              <div />
+              <div />
+              <div />
+            </div>
 
-    <div className="col-12 button-holder">
-      <a href="/Categories">Start Now</a>
-    </div>
+            <div className="col-12 title-row">
+              <h1>
+                <mark>Create Your Beautiful Wedding </mark>
+              </h1>
+              <h4>
+                <mark>Be Your Wedding Planner</mark>
+              </h4>
 
-  </div>
-</div>
-</header>
-<About/>
-</div>
-    )
-}
+              <div className="col-12 button-holder">
+                <a href="/Budget">Start Now</a>
+              </div>
+            </div>
+          </div>
+        </header>
+        <About />
+      </div>
+    );
+  }
 }
 export default Home;

7964c8ec47861e545577b9d151b31974a10d1743
ashraf jibreel <ashrafjebril9@gmail.com>
2018-12-24 19:51:11 +0200

    (refactor add categories)

--- DataBase/schema.sql
+++ DataBase/schema.sql
@@ -4,100 +4,62 @@ CREATE DATABASE sql2270948;
 
 USE sql2270948;
 
-DROP TABLE IF EXISTS `ProviderCatergory`;
-DROP TABLE IF EXISTS `User`;
-DROP TABLE IF EXISTS `Provider`;
-DROP TABLE IF EXISTS `Category`;
--- ---
--- Table 'User'
--- 
--- ---		
-CREATE TABLE `User`
-(
+CREATE TABLE `Users` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`password` varchar
-(255) NOT NULL,
-	`name` varchar
-(50) NOT NULL,
-    `email` varchar
-(50) NOT NULL,
-	PRIMARY KEY
-(`id`)
+	`password` varchar(255) NOT NULL,
+	`name` varchar(50) NOT NULL,
+	`email` varchar(70) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
--- ---
--- Table 'Provider'
--- 
--- ---
-
-
-CREATE TABLE `Provider`
-(
+CREATE TABLE `Providers` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`name` varchar
-(50) NOT NULL,
-	`password` varchar
-(255) NOT NULL,
-    `email` varchar
-(50) NOT NULL,
-	PRIMARY KEY
-(`id`)
+	`name` varchar(50) NOT NULL,
+	`password` varchar(255) NOT NULL,
+	`email` varchar(70) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
--- ---
--- Table 'Category'
--- 
--- ---
-
-
-
-CREATE TABLE `Category`
-(
-	`id` int
-(70) NOT NULL AUTO_INCREMENT,
-    `name` varchar
-(70) NOT NULL,
-	`type` int
-(10) NOT NULL,
-	PRIMARY KEY
-(`id`)
+CREATE TABLE `Categories` (
+	`name` varchar(70) NOT NULL,
+	`id` int(70) NOT NULL AUTO_INCREMENT,
+	`type` int(10) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
--- ---
--- Table 'ProviderCatergory'
--- 
--- ---
-
-
-
-CREATE TABLE `ProviderCatergory`
-(
+CREATE TABLE `ProviderCatergories` (
 	`id` int NOT NULL AUTO_INCREMENT,
 	`price` int NOT NULL,
-	`description` varchar
-(70) NOT NULL,
-	`providerID` int
-(70) NOT NULL,
-	`categoryID` int
-(70) NOT NULL,
-	`capacity` int
-(70),
-	PRIMARY KEY
-(`id`)
+	`description` varchar(70) NOT NULL,
+	`providerID` int(70) NOT NULL,
+	`categoryID` int(70) NOT NULL,
+	`capacity` int(70),
+	`location` varchar(255),
+	`imageUrl` varchar(255),
+	`title` varchar(255) NOT NULL,
+	`rate` int NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
--- ---
--- Foreign Keys 
--- ---
-ALTER TABLE `ProviderCatergory`
-ADD CONSTRAINT `ProviderCatergory_fk0` FOREIGN KEY
-(`providerID`) REFERENCES `Provider`
-(`id`);
+CREATE TABLE `Reservations` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`UserID` int(70) NOT NULL,
+	`ProviderID` int(70) NOT NULL,
+	`ProCateID` int(70) NOT NULL,
+	`` bigint NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-ALTER TABLE `ProviderCatergory`
-ADD CONSTRAINT `ProviderCatergory_fk1` FOREIGN KEY
-(`categoryID`) REFERENCES `Category`
-(`id`);
+ALTER TABLE `ProviderCatergories` ADD CONSTRAINT `ProviderCatergories_fk0` FOREIGN KEY (`providerID`) REFERENCES `Providers`(`id`);
+
+ALTER TABLE `ProviderCatergories` ADD CONSTRAINT `ProviderCatergories_fk1` FOREIGN KEY (`categoryID`) REFERENCES `Categories`(`id`);
+
+ALTER TABLE `Reservations` ADD CONSTRAINT `Reservations_fk0` FOREIGN KEY (`UserID`) REFERENCES `Users`(`id`);
+
+ALTER TABLE `Reservations` ADD CONSTRAINT `Reservations_fk1` FOREIGN KEY (`ProviderID`) REFERENCES `Providers`(`id`);
+
+ALTER TABLE `Reservations` ADD CONSTRAINT `Reservations_fk2` FOREIGN KEY (`ProCateID`) REFERENCES `ProviderCatergories`(`id`);
+
 
 
 

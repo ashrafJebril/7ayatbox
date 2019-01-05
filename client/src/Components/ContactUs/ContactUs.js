414c73758b5b783f95197d6e0a2c8ec8ee2c59a2
yazeedhammad <yazeedhammad91@gmail.com>
2019-01-05 19:49:26 +0200

    (feat)about us page

--- client/src/Components/ContactUs/ContactUs.js
+++ client/src/Components/ContactUs/ContactUs.js
@@ -49,12 +49,35 @@ class ContactUs extends Component {
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
         <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-        <div className="aboutus_container">
-          <h1 className="contactus_about_title">About Us</h1>
-          <div className="contactus_about_div">
-            <p className="contactus_about">An About Us Page is a page on your website that tells your readers all about you. It includes a detailed description covering all aspects of your business and you as entrepreneurs. This includes the products or services you are offering, how you came into being as a business, your mission and vision, your aim, and maybe something about your future goals too. Your “About Us” page is your perfect opportunity to tell a compelling story about yourself and your business.</p>
-          </div>
-        </div>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
+
+        {/* About Us Form  */}
+        <section>
+	<div class="container-fluid">
+        	<div class="row about-bg">
+                <div class="col-md-6">   
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12 about-bg-layer">
+                <div class="">
+                <div class="about-content-box">
+                <div class="about-heading">
+                    <h1>ABOUT US</h1>
+                    <h2>Hayat Box</h2>
+                    <p>Wedding Planner</p>
+                </div>
+                    <div class="about-txt">
+                    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br/>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
+                    </div>
+                </div>
+			</div>
+        </div> 
+    </div>
+</section>
+        {/* End Of About Us form */}
 
         {/* Contact Us Form */}
         <div class="contact-jumbotron contact-jumbotron-sm">
@@ -71,19 +94,19 @@ class ContactUs extends Component {
     <div class="row">
         <div class="col-md-8">
             <div class="well well-sm">
-                <form>
+                <form onSubmit={this.handleSaveMessage}>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="name">
                                 Name</label>
-                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                            <input type="text" class="form-control" name="name" placeholder="Enter name" required="required" onChange={this.nameChange} />
                         </div>
 
                         <div class="form-group">
                             <label for="subject">
                                 Phone</label>
-                            <input type="text" class="form-control" id="phone" placeholder="Enter phone" required="required" />
+                            <input type="text" class="form-control" name="phone" placeholder="Enter phone" required="required" onChange={this.phoneChange}/>
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -91,7 +114,7 @@ class ContactUs extends Component {
                             <label for="name">
                                 Message</label>
                             <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                placeholder="Message"></textarea>
+                                placeholder="Message" onChange={this.messageChange}></textarea>
                         </div>
                     </div>
                     <div class="col-md-12">
